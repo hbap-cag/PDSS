13ccd7e836ca0a5e33c2ac5d2df5e64d25c78383
hbap-cag <christian.a.guerra@mail.analytics.hbs.edu>
2019-10-23 22:01:31 -0500

    Quote.py completed

--- quote.py
+++ quote.py
@@ -2,12 +2,14 @@ import csv
 import requests
 import sys
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 if len(sys.argv) != 2:
     sys.exit("Usage: py quote.py SYMBOL")
 symbol = sys.argv[1]
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv('API_KEY')
 if not API_KEY:
     sys.exit("Missing API_KEY")
 url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&datatype=csv"
