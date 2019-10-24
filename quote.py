4793c7319dd40f80076a2c100fab9c63a026aa39
hbap-cag <christian.a.guerra@mail.analytics.hbs.edu>
2019-10-23 20:56:42 -0500

    Uploading

---
+++ quote.py
@@ -0,0 +1,19 @@
+import csv
+import requests
+import sys
+import os
+
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: py quote.py SYMBOL")
+symbol = sys.argv[1]
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    sys.exit("Missing API_KEY")
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+response = requests.get(url)
+
+reader = csv.DictReader(response.text.splitlines())
+
+row = next(reader)
+print(f"${row['close']}")
